795711cf9531469818829d93e8946d6208536c8d
explorer1500 <163674076+explorer7733@users.noreply.github.com>
2024-09-04 15:01:30 -0700

    install Apollo server, modify the server.js file to apply Apollo server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const path = require('path');
+
+const { AppoloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+const { authMiddleware } = require('./utils/auth');
+
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+// Apollo server setup
+const server = new AppoloServer({
+  typeDefs,
+  resolvers,
+});
+const startApolloServer = async () => {
+  await server.start(); // start Apollo server
+// Apply the Apollo GraphQL middleware and set the path to /graphql
+  app.use(
+    '/graphql',
+    expressMiddleware(server, {
+      context: authMiddleware,
+    })
+  );
+};
+startApolloServer();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -13,8 +34,6 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
-
-app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
