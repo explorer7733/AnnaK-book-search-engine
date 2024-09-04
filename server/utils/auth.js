f3e994a31bd7cb44b735271691dfa1a14c27585b
explorer1500 <163674076+explorer7733@users.noreply.github.com>
2024-09-04 13:44:11 -0700

    update the auth middleware function in auth.js file to work with GraphQL API

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,3 +1,4 @@
+const { GraphQLError } = require('graphql');
 const jwt = require('jsonwebtoken');
 
 // set token secret and expiration date
@@ -6,9 +7,9 @@ const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
+  authMiddleware: function ({req}) {
     // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    let token = req.body.token ||req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
@@ -16,7 +17,10 @@ module.exports = {
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+     throw new GraphQLError('No token found', {
+       extensions: { 
+        code: 'UNAUTHENTICATED' },
+     })
     }
 
     // verify token and get user data out of it
@@ -25,12 +29,16 @@ module.exports = {
       req.user = data;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+
+      throw new GraphQLError('Invalid token', {
+        extensions: { 
+          code: 'UNAUTHENTICATED' },
+      });
     }
 
-    // send to next endpoint
-    next();
+    return req; 
   },
+
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
 
